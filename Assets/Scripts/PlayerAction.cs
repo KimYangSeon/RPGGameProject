d8540c9deb0e82a54ca1a9c0fda675c0962692c8
KimYangSeon <endol_@naver.com>
2022-12-16 13:36:49 +0900

    플레이어 애니메이션 + Raycast

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -8,14 +8,14 @@ public class PlayerAction : MonoBehaviour
     float v;
     public float velocity;
     Rigidbody2D rigid;
+    Animator anim;
+    Vector3 dirVec;
+    GameObject scanObject;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-    }
-    void Start()
-    {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,10 +23,40 @@ public class PlayerAction : MonoBehaviour
     {
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
+
+        if (anim.GetInteger("hAxisRaw") != h) {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("hAxisRaw", (int)h);
+        }
+        else if(anim.GetInteger("vAxisRaw") != v)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("vAxisRaw", (int)v);
+        }
+        else
+        {
+            anim.SetBool("isChange", false);
+        }
+
+        if (h != 0 || v != 0) dirVec = new Vector2(h, v).normalized;
+
+        // scan object
+        if (Input.GetKeyDown(KeyCode.Z) && scanObject) Debug.Log(scanObject);
+
+
     }
 
     void FixedUpdate()
     {
-        rigid.velocity = new Vector2(h, v)*Time.deltaTime* velocity;
+        rigid.velocity = new Vector2(h, v).normalized * Time.deltaTime * velocity;
+
+        Debug.DrawRay(rigid.position, dirVec * 0.7f, new Color(0, 1, 0));
+        // Object 레이어만 스캔
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, dirVec, 0.7f, LayerMask.GetMask("Object"));
+        if (rayHit.collider)
+        {
+            scanObject = rayHit.collider.gameObject;
+        }
+        else scanObject = null;
     }
 }
