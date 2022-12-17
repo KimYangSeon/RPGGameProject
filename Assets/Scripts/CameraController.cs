8a6a440dd33c5168eac646ec65c417bf3168ca87
KimYangSeon <endol_@naver.com>
2022-12-17 18:19:11 +0900

    카메라 스크립트 추가

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{ 
+    [SerializeField]
+    Transform playerTransform;
+    [SerializeField]
+    Vector2 mapSize;
+    [SerializeField]
+    RectTransform map;
+
+    Vector3 cameraPos = new Vector3(0, 0, -10);
+
+    float height;
+    float width;
+    float minX;
+    float minY;
+
+    void Start()
+    {
+        height = Camera.main.orthographicSize;
+        width = height * Screen.width / Screen.height;
+
+        minX = mapSize.x - width;
+        minY = mapSize.y - height;
+    }
+    void FixedUpdate()
+    {
+        transform.position = playerTransform.position + cameraPos;
+
+        float clampX = Mathf.Clamp(transform.position.x, -minX, minX);
+        float clampY = Mathf.Clamp(transform.position.y, -minY, minY);
+
+        transform.position = new Vector3(clampX, clampY, -10f);
+    }
+}
