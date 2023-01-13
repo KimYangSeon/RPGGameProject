350c0bdf913656ab1c44c068206b50501a4e1613
KimYangSeon <endol_@naver.com>
2023-01-13 15:23:51 +0900

    플레이어 에셋 변경 + 보스 스프라이트 추가

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -21,9 +21,10 @@ public class PlayerAction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 플레이어 이동  
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
-
+        /*
         if (anim.GetInteger("hAxisRaw") != h) {
             anim.SetBool("isChange", true);
             anim.SetInteger("hAxisRaw", (int)h);
@@ -37,20 +38,29 @@ public class PlayerAction : MonoBehaviour
         {
             anim.SetBool("isChange", false);
         }
-
-        if (h != 0 || v != 0) dirVec = new Vector2(h, v).normalized;
+        */
+        if (h != 0 || v != 0)
+        {
+            anim.SetBool("isChange", true);
+            dirVec = new Vector2(h, v).normalized;
+        }
+        else anim.SetBool("isChange", false);
 
         // scan object
-        if (Input.GetKeyDown(KeyCode.Z) && scanObject) Debug.Log(scanObject);
+        if (Input.GetKeyDown(KeyCode.X) && scanObject) Debug.Log(scanObject);
 
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            anim.SetTrigger("Attack");
+        }
 
     }
 
     void FixedUpdate()
     {
-        rigid.velocity = new Vector2(h, v).normalized * Time.deltaTime * velocity;
+        rigid.velocity = new Vector2(h, v).normalized * Time.deltaTime * velocity; // 플레이어 이동
 
-        Debug.DrawRay(rigid.position, dirVec * 0.7f, new Color(0, 1, 0));
+        Debug.DrawRay(rigid.position, dirVec * 0.7f, new Color(0, 1, 0)); // 레이캐스트
         // Object 레이어만 스캔
         RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, dirVec, 0.7f, LayerMask.GetMask("Object"));
         if (rayHit.collider)
@@ -58,5 +68,7 @@ public class PlayerAction : MonoBehaviour
             scanObject = rayHit.collider.gameObject;
         }
         else scanObject = null;
+
+
     }
 }
