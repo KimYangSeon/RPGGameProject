9ff3ab4c5cfe0fd2767ae7082a236f98f832f633
KimYangSeon <endol_@naver.com>
2023-03-25 17:28:51 +0900

    보스 패턴 추가 & 이동 로직 변경

--- Assets/Scripts/BossAction.cs
+++ Assets/Scripts/BossAction.cs
@@ -12,6 +12,11 @@ public class BossAction : MonoBehaviour
     public float skillRange;
     //public float speed;
     public Transform player;
+    public GameObject alert;
+    bool isAttacking = false;
+    Vector3 velocity;
+
+    WaitForFixedUpdate fixedUpdate = new WaitForFixedUpdate();
     void Awake()
     {
         anim = GetComponent<Animator>();
@@ -19,28 +24,84 @@ public class BossAction : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(bossMove());
+        //StartCoroutine(bossMove());
+        velocity = Vector3.zero;
+        Invoke("choosePattern", 5f);
+
     }
 
     void Update()
     {
+        if (!isAttacking)
+        {
+            transform.position
+                    = Vector3.SmoothDamp(transform.position, player.position, ref velocity, 1.8f);
+        }
+        
+    }
 
+    void choosePattern()
+    {
+        bossPattern(0);
+    }
+
+    void bossPattern(int patternIdx)
+    {
+        switch (patternIdx)
+        {
+            case 0:
+                StartCoroutine(defaultAttack());
+                break;
+        }
+    }
+
+    IEnumerator defaultAttack()
+    {
+        isAttacking = true;
+        alert.SetActive(true);
+        yield return StartCoroutine(delay(3));
+        Debug.Log("공격");
+        alert.SetActive(false);
+        isAttacking = false;
+
+        Invoke("choosePattern", 10);
+    }
+
+    IEnumerator delay(float delayTime)
+    {
+        float cur = 0;
+        while (true)
+        {
+            if (cur < delayTime)
+            {
+                yield return fixedUpdate;
+                cur += Time.deltaTime;
+            }
+            else
+            {
+                yield break;
+            }
+        }
     }
 
     IEnumerator bossMove()
     {
         while (true)
         {
+            
             Vector3 velocity = Vector3.zero;
             float distance = Vector2.Distance(transform.position, player.position);
             yield return new WaitForSeconds(1.5f);
             while (distance > skillRange)
             {
+                if (isAttacking) continue;
+
                 transform.position
                     = Vector3.SmoothDamp(transform.position, player.position, ref velocity, 1.8f);
-
+                distance = Vector2.Distance(transform.position, player.position);
                 yield return null;
             }
+            //Debug.Log("이동 끝");
             yield return null;
         }
     }
